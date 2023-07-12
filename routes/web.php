e00642f5f856b7a76c2520ba85210a9fe394ac8e
Imamabdullah71 <imamabdullah0902@gmail.com>
2023-07-12 15:57:02 +0700

    Menggabungkan AdminLTE dengan Laravel, masih mencari kesalahan pada crud table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\sumberDanaZakatController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,21 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/sumberDana', function () {
-    return view('sumberDana.index');
-});
+// Route::get('/admin', function () {
+//     return view('admin.index');
+// });
 
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::get('/', [HomeController::class, 'dashboard']);
 
-Route::resource('sumberDanaZakat',sumberDanaZakatController::class);
+Route::get('/user', [HomeController::class, 'index']);
+
+Route::get('/create', [HomeController::class, 'create'])->name('user.create');
+
+Route::post('/store', [HomeController::class, 'store'])->name('user.store');
