d7370b84f1effb6c1a0cf1eac4ea3bb92e22e57e
Imamabdullah71 <imamabdullah0902@gmail.com>
2023-08-01 19:07:45 +0700

    Memperbaharui landing page

--- resources/views/page/welcomePage.blade.php
+++ resources/views/page/welcomePage.blade.php
@@ -5,28 +5,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selamat Datang</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .carousel-inner img {
-            object-fit: cover;
-            width: 900px;
-            height: 500px;
-        }
-    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/welcomepagestyle.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
-    <div class="jumbotron jumbotron-fluid bg-primary text-white">
-        <div class="container d-flex justify-content-center">
-            <h1 class="display-4"><b>Selamat Datang</b></h1>
-        </div>
-        <div class="container d-flex justify-content-center">
-            <a href="{{ route('login') }}" class="btn btn-success">Masuk ke Login</a>
-        </div>
-    </div>
 
-    <div class="container">
+    <header>
+        <img src="{{ asset('images/LogoKilau.png') }}" alt="" class="logo-kilau">
+
+        <nav class="navtop">
+            <a href="#">Home</a>
+            <a href="#">Event</a>
+            <a href="#">Venue</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+
+        <i class="fa-solid fa-moon mode"></i>
+    </header>
+
+    <section class="home">
+        <div class="contenthome">
+            <h3>Lembaga<br><span>Kilau</span><br>Berbagi Teknologi</h3>
+            <p>
+                Jules Bianchi (1989–2015) adalah seorang pembalap mobil profesional dari Prancis. Ia pernah berkompetisi dalam ajang Formula Satu (F1) pada musim 2013 dan 2014 bersama tim Marussia. Bianchi memulai karier membalapnya pada usia 10 tahun dengan mengikuti kejuaraan gokar di Prancis dan berhasil meraih beberapa kesuksesan awal, di antaranya menjadi juara Kejuaraan Junior Prancis dan peringkat kedua dalam Kejuaraan Junior Eropa pada tahun 2004. Selanjutnya, ia mengikuti berbagai ajang kejuaraan junior seperti Formula 3 Euro Series, Seri GP2 dan Formula Renault 3.5. Pada tahun 2009, Bianchi menjadi pembalap pertama yang bergabung dengan Akademi Pembalap Ferrari, sebuah program pembinaan pembalap dari tim Scuderia Ferrari yang bertujuan untuk mendidik seorang pembalap muda untuk nantinya bisa berkiprah
+            </p>
+            <a href="#" class="tombol"><span></span>Read More</a>
+            <div class="iconsosmed">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+        </div>
+
+        <div class="gambar">
+            <img src="{{ asset('images/LogoKilau3.png') }}" alt="" class="siluet-kilau">
+            <img src="{{ asset('images/LogoKilau2.png') }}" alt="" class="logokilau2">
+        </div>
+        <img src="{{ asset('images/boderkilau.png') }}" alt="" class="border">
+    </section>
+
+    {{-- <div class="jumbotron jumbotron-fluid bg-primary text-white">
+        <div class="container d-flex justify-content-center">
+            <h1 class="display-4 pb-5"><b>Selamat Datang</b></h1>
+        </div>
+        <div class="container d-flex justify-content-center">
+            <a href="{{ route('login') }}" class="btnmsk">Masuk ke Login</a>
+        </div>
+    </div> --}}
+
+    {{-- <div class="container">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -65,12 +95,12 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-    </div>
+    </div> --}}
 
+    <script src="{{ asset('javascript/welcomepage.js') }}"></script>
     <!-- Bootstrap JS (jquery and popper.js are required for Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
