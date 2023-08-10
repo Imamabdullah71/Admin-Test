56cc65490b413d551d79bac8a2b0e0bbb4b9d183
Imamabdullah71 <imamabdullah0902@gmail.com>
2023-08-10 17:03:33 +0700

    Melanjutkan merapihkan login page dan register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,87 +31,32 @@
       </div>
     </div>
     <div class="login">
-      <h2>Sign In</h2>
-      <div class="inputBox">
-        <input type="text" placeholder="Username">
-      </div>
-      <div class="inputBox">
-        <input type="password" placeholder="Password">
-      </div>
-      <div class="inputBox">
-        <input type="submit" value="Login" id="tombollogin">
-      </div>
-      <div class="group">
-        <a href="#"> Belum Punya Akun?</a>
-        <a href="#">Register</a>
-      </div>
-    </div>
-  </section>
-
-
-{{-- <div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="{{ route('login') }}" class="h1">Admin Login</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Login untuk masuk ke Dashboard</p>
-
+      <h2>Login</h2>
       <form action="{{ route('login-proses') }}" method="post">
         @csrf
-        <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+        <div class="inputBox">
+          <input type="email" name="email" placeholder="Email">
         </div>
         @error('email')
         <small>{{ $message }}</small>
         @enderror
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
+        <div class="inputBox">
+          <input type="password" name="password" placeholder="Password">
         </div>
         @error('password')
         <small>{{ $message }}</small>
         @enderror
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
+        <div class="inputBox">
+          <input type="submit" value="Login" id="tombollogin">
         </div>
       </form>
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <br>
-      <p class="mb-0">
-        Belum memiliki akun? <a href="{{ route('register') }}" class="text-center">Register</a>
-      </p>
+      <div class="group">
+        <a href="#"> Belum Punya Akun?</a>
+        <a href="{{ route('register') }}">Register</a>
+      </div>
     </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box --> --}}
+  </section>
+  
 <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js" integrity="sha512-/6TZODGjYL7M8qb7P6SflJB/nTGE79ed1RfJk3dfm/Ib6JwCT4+tOfrrseEHhxkIhwG8jCl+io6eaiWLS/UX1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
   let scene = document.getElementById('scene');
--- resources/views/layout/contoh.blade.php
+++ resources/views/layout/contoh.blade.php
@@ -1,91 +1,104 @@
-//resources/views/home.blade.php
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-    <title>Laravel 10 Delete Multiple Data using Checkbox | Jquery Ajax</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Register</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
 </head>
-<body>
-    <div class="container">
-        @if ($message = Session::get('success'))
-        <div class="alert alert-info">
-            <p>{{ $message }}</p>
-        </div>
-        @endif
-        <h2 class="mb-4">Laravel 10 Delete Multiple Data using Checkbox | Jquery Ajax</h2>
-        <button class="btn btn-primary btn-xs removeAll mb-3">Remove All Selected Data</button>
-        <table class="table table-bordered">
-            <tr>
-                <th><input type="checkbox" id="checkboxesMain"></th>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Email</th>
-            </tr>
-            @if($users->count())
-            @foreach($users as $key => $rs)
-            <tr id="tr_{{$rs->id}}">
-                <td><input type="checkbox" class="checkbox" data-id="{{$rs->id}}"></td>
-                <td>{{ ++$key }}</td>
-                <td>{{ $rs->name }}</td>
-                <td>{{ $rs->email }}</td>
-            </tr>
-            @endforeach
-            @endif
-        </table>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="{{ route('register') }}" class="h1">Register</a>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
-<script type = "text/javascript" >
-    $(document).ready(function() {
-        $('#checkboxesMain').on('click', function(e) {
-            if ($(this).is(':checked', true)) {
-                $(".checkbox").prop('checked', true);
-            } else {
-                $(".checkbox").prop('checked', false);
-            }
-        });
-        $('.checkbox').on('click', function() {
-            if ($('.checkbox:checked').length == $('.checkbox').length) {
-                $('#checkboxesMain').prop('checked', true);
-            } else {
-                $('#checkboxesMain').prop('checked', false);
-            }
-        });
-        $('.removeAll').on('click', function(e) {
-            var userIdArr = [];
-            $(".checkbox:checked").each(function() {
-                userIdArr.push($(this).attr('data-id'));
-            });
-            if (userIdArr.length <= 0) {
-                alert("Choose min one item to remove.");
-            } else {
-                if (confirm("Are you sure you want to delete")) {
-                    var stuId = userIdArr.join(",");
-                    $.ajax({
-                        url: "{{url('delete-all')}}",
-                        type: 'DELETE',
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        data: 'ids=' + stuId,
-                        success: function(data) {
-                            if (data['status'] == true) {
-                                $(".checkbox:checked").each(function() {
-                                    $(this).parents("tr").remove();
-                                });
-                                alert(data['message']);
-                            } else {
-                                alert('Error occured.');
-                            }
-                        },
-                        error: function(data) {
-                            alert(data.responseText);
-                        }
-                    });
-                }
-            }
-        });
-    }); 
+    <div class="card-body">
+      <p class="login-box-msg">Register Account</p>
+
+      <form action="{{ route('register-proses') }}" method="post">
+        @csrf
+        <div class="input-group mb-3">
+            <input type="text" name="nama" class="form-control" placeholder="Nama Lengkap..." value="{{ old('nama') }}">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+          </div>
+          @error('nama')
+          <small>{{ $message }}</small>
+          @enderror
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        @error('email')
+        <small>{{ $message }}</small>
+        @enderror
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        @error('password')
+        <small>{{ $message }}</small>
+        @enderror
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <br>
+      <p class="mb-0">
+        Sudah memiliki akun? <a href="{{ route('login') }}" class="text-center">Login</a>
+      </p>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('lte/dist/js/adminlte.min.js') }}"></script>
+
+<!-- SweetAlert2 -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@if ($message = Session::get('success'))
+<script>
+  Swal.fire('{{ $message }}')
 </script>
+@endif
+
+@if ($message = Session::get('failed'))
+<script>
+  Swal.fire('{{ $message }}')
+</script>
+@endif
+
 </body>
 </html>
